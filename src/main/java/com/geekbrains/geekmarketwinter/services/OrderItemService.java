7f4fcfb73732d933d02e0b6fa54c3baa7aa67be4
shein dima <msh86.02.18@rambler.ru>
2020-06-08 23:21:02 +0500

    Добавил интерфейс и провайдер для OrderItem

--- src/main/java/com/geekbrains/geekmarketwinter/services/OrderItemService.java
+++ src/main/java/com/geekbrains/geekmarketwinter/services/OrderItemService.java
@@ -1,21 +1,21 @@
 package com.geekbrains.geekmarketwinter.services;
 
 import com.geekbrains.geekmarketwinter.entites.OrderItem;
+import com.geekbrains.geekmarketwinter.interfaces.IOrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderItemService {
-    private OrderItemRepository orderItemRepository;
+    private IOrderItem orderItems;
 
     @Autowired
-    public OrderItemService(OrderItemRepository orderItemRepository) {
-        this.orderItemRepository = orderItemRepository;
+    public OrderItemService(IOrderItem orderItem) {
+        this.orderItems = orderItem;
     }
 
-
     public void save(OrderItem orderItem) {
-        orderItemRepository.save(orderItem);
+        orderItems.save(orderItem);
     }
 
 
